11509eb9c2b4937586431265b45446a4b8d1bd65
facundo <facundo915@gmail.com>
2018-03-12 13:57:55 -0300

    Add Test

---
+++ test/TP01/Ej1Test.java
@@ -0,0 +1,35 @@
+package TP01;
+
+import java.util.Arrays;
+
+import static TP01.Ej1.*;
+import static org.junit.Assert.*;
+
+public class Ej1Test {
+
+
+    @org.junit.Test
+    public void binSearchTest() {
+        int[] array = {1,4,6,8,10,12,14};
+        assertEquals(4,binSearch(array,0,array.length,10));
+    }
+
+    @org.junit.Test
+    public void seqSearch() {
+        int[] array = {1,4,6,8,10,12,14};
+        assertEquals(4,binSearch(array,0,array.length,10));
+    }
+
+    @org.junit.Test
+    public void selectionSortTest() {
+        int[] array = {6,8,1,4,12,10,14};
+        selectionSort(array);
+        assertEquals("[1, 4, 6, 8, 10, 12, 14]",Arrays.toString(array));
+    }
+    @org.junit.Test
+    public void insertionSortTest(){
+        int[] array = {6,8,1,4,12,10,14};
+        insertionSort(array);
+        assertEquals("[1, 4, 6, 8, 10, 12, 14]",Arrays.toString(array));
+    }
+}
