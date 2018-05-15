e41e44574b958d698083be436293db1a6c3c6349
FDGonzalez <facundo915@gmail.com>
2018-05-15 09:15:10 -0300

    added constructor

--- src/tp04simulaciondecolas/GUI/Controller.java
+++ src/tp04simulaciondecolas/GUI/Controller.java
@@ -6,12 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import tp04simulaciondecolas.Supermarket;
 
 public class Controller extends Application {
 
     private Stage primaryStage;
     private BorderPane mainLayaut;
-
+    private Supermarket supermarket;
+    private int simulationLength;
+    private int n;
+    private int ms;
+    public Controller(Supermarket supermarket, int simulationLength, int n, int ms){
+        this.supermarket = supermarket;
+        this.simulationLength = simulationLength;
+        this.n = n;
+        this.ms = ms;
+    }
 
    public void start(){
        launch();
